25a8a6209940b44398732888e6fb84619361574a
Kim Mi Reu <samkmr0220@gmail.com>
2022-07-24 16:01:14 +0900

    implement-login-modal

--- login.js
+++ login.js
@@ -1,17 +1,40 @@
 const loginNav = $('.loginNav');
 const login = $('.login');
 const baduk = $('.baduk');
-
-const id = $('#id');
+const login_id = $('#login-id');
 const loginForm = $('#login-form');
+const modal = document.getElementById("modal")
+const content = $('.content');
+const closeBtn = modal.querySelector(".close-area")
 
 loginForm.submit(onLoginSubmit);
 
 function onLoginSubmit(event) {
   event.preventDefault();
+  const userId = login_id.val();
+  content.text(`${userId}님 환영합니다!`);
+  modal.style.display = "flex"
   login.addClass('hidden');
   loginNav.addClass('hidden');
-  baduk.removeAttr('id');
-  const username = id.val();  
-  alert(`${username}님, 환영합니다!`);
+
 }
+
+closeBtn.addEventListener("click", e => {
+  modal.style.display = "none"
+  baduk.removeAttr('id'); 
+})
+
+modal.addEventListener("click", e => {
+  const evTarget = e.target
+  if(evTarget.classList.contains("modal-overlay")) {
+    modal.style.display = "none"
+    baduk.removeAttr('id');
+  }
+})
+
+window.addEventListener("keyup", e => {
+  if(modal.style.display === "flex" && e.key === "Escape") {
+    modal.style.display = "none"
+    baduk.removeAttr('id');
+  }
+})
