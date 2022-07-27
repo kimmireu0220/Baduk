7d3053758ac1e03f404adba12a0a25a4dc441d9c
Kim Mi Reu <samkmr0220@gmail.com>
2022-07-27 21:44:30 +0900

    modify-login

--- login.js
+++ login.js
@@ -9,34 +9,62 @@ const modal = document.getElementById("modal")
 const content = $('.content');
 const closeBtn = modal.querySelector(".close-area")
 
+const modalTitle = $('.modalTitle');
+
+let loginSuccess;
+
 loginForm.submit(onLoginSubmit);
 
 function onLoginSubmit(event) {
   event.preventDefault();
   const userId = login_id.val();
-  content.text(`${userId} 님 환영합니다!`);
-  modal.style.display = "flex"
-  login.addClass('hidden');
-  loginNav.addClass('hidden');
-
+  if (userId) {
+    modalTitle.text('로그인 성공');
+    content.text(`${userId} 님 환영합니다!`);
+    modal.style.display = "flex"
+    login.addClass('hidden');
+    loginNav.addClass('hidden');
+    loginSuccess = true;
+  }
+  else {
+    modalTitle.text('로그인 실패');
+    content.text('아이디를 입력해주세요!!');
+    modal.style.display = "flex"
+    loginSuccess = false;
+  }
 }
 
 closeBtn.addEventListener("click", e => {
-  modal.style.display = "none"
-  baduk.removeAttr('id'); 
+  if(loginSuccess) {
+    modal.style.display = "none"
+    baduk.removeAttr('id');
+  }
+  else {
+    modal.style.display = "none"
+  }
 })
 
 modal.addEventListener("click", e => {
   const evTarget = e.target
   if(evTarget.classList.contains("modal-overlay")) {
-    modal.style.display = "none"
-    baduk.removeAttr('id');
+    if(loginSuccess) {
+      modal.style.display = "none"
+      baduk.removeAttr('id');
+    }
+    else {
+      modal.style.display = "none"
+    }
   }
 })
 
 window.addEventListener("keyup", e => {
   if(modal.style.display === "flex" && e.key === "Escape") {
-    modal.style.display = "none"
-    baduk.removeAttr('id');
+    if(loginSuccess) {
+      modal.style.display = "none"
+      baduk.removeAttr('id');
+    }
+    else {
+      modal.style.display = "none"
+    }
   }
 })
